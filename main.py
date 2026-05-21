13a197cf7ffc5c6116664a961f92a78c738f7c73
Tirezada <christian.espindula@gmail.com>
2026-05-21 16:09:36 -0300

    Análise de Jogadores Fictícios

---
+++ main.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+df_stats = pd.read_excel('estatisticas_jogadores.xlsx', sheet_name='Jogadores', header=1, skipfooter=1)
+df_resum = pd.read_excel('estatisticas_jogadores.xlsx', sheet_name='Resumo por Posição', header=1)
+df_strikers = pd.read_excel('estatisticas_jogadores.xlsx', sheet_name='Top 10 Artilheiros', header=1)
+
+def analise_grupo(coluna, valor):
+    return df_stats.groupby(coluna)[valor].sum().sort_values(ascending = False)
+
+
+colunas = [
+    'Jogos', 'Gols', 'Assistências', 'Chutes no Gol',
+    'Dribles Certos', 'Cartões Amarelos', 'Min. Jogados',
+    'Altura (cm)', 'Peso (kg)' 
+]
+
+for col in colunas:
+    total = analise_grupo('Posição', col)
+    media = (total / df_stats['Posição'].value_counts()).sort_values(ascending=False)
+    
+    print(f'--- {col} por Posição ---')
+    print(total.to_string())
+    print(f'--- Média de {col} por Posição ---')
+    print(media.to_string())
+
+
